40289f3fd5800807ae2831ec2895f2b5d13d5f91
Eri <erysy77@gmail.com>
2025-01-25 17:47:01 -0300

    Adicionar função gerarSorteio() e função resultado

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Armazenar os nomes em um Array, adicionar e listar os amigos com uma iteração de array.
 
 let listaAmigos = [];
+let listaSorteados = [];
+
+let indice = Math.floor(Math.random() * listaAmigos.length)
 
 
 function limparCampo(){
@@ -27,10 +30,51 @@ function exibirLista(){
     let listar = document.getElementById('listaAmigos');
     listar.innerHTML = '';
 
-    listaAmigos.forEach(function (amigo){
+
+    listaAmigos.forEach(function (amigo, index){ // Iteração para cada Amigo na lista.
         let elemento = document.createElement('li');
-        elemento.textContent = amigo;
+        elemento.textContent = `${index+1}. `+ amigo;
         listar.appendChild(elemento);
     });
-   
+
 }
+
+
+function resultado(mensagem){
+    const resultado = document.getElementById('resultado');
+    resultado.textContent = mensagem;
+}
+
+
+function gerarSorteio(){
+    if (listaAmigos.length === 0 ){
+        alert('A lista de amigos está vazia');
+        return;
+    }
+    // Verificando se todos foram sorteados.
+    if (listaSorteados.length == listaAmigos.length){
+        console.log('Todos já foram sorteados');
+        resultado('Todos já foram sorteados');
+        return;
+    }
+
+    let sorteado;
+    // O loop abaixo irá garantir que não haja sorteios duplicados.
+    do{
+        let indice = Math.floor(Math.random() * listaAmigos.length);
+        sorteado = listaAmigos[indice];
+    }while (listaSorteados.includes(sorteado));
+
+    //Adiciona o amigo sorteado na lista dos sorteados.
+    listaSorteados.push(sorteado);
+
+    resultado(`Amigo sorteado: ${sorteado}`);
+    console.log(listaSorteados);
+}
+
+
+function sortearAmigo(){
+    gerarSorteio();
+}
+
+
